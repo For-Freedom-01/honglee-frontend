cb4d0a07870c8a4eb46e4d23cdc2ea8d252df23c
hongseok <hghg990124@naver.com>
2025-01-19 20:48:10 +0900

    create-loginForm

--- react-app/src/component/Login.js
+++ react-app/src/component/Login.js
@@ -1,14 +1,39 @@
+import { useRef } from "react"
+import {useNavigate } from "react-router-dom"
+
 export default function Login(){
+  const refId = useRef(null)
+  const refPassword = useRef(null)
+  const nav = useNavigate()
+  
+
+  
+  async function onLogin(e){
+    e.preventDefault()
+    const id = refId.current.value.trim()
+    const password = refPassword.current.value.trim()
+    console.log(id,password)
+    await fetch(`http://localhost:3001/0?id=${id}&password=${password}`)
+    .then(data=> {
+      if (!data.ok) {
+        window.alert("id 또는 password를 확인해 주세요")
+      } else {
+        window.alert("로그인에 성공하셨습니다")
+        nav('/')
+      }
+    })
+    .catch(error => console.log(error))
+  }
   return(
     <>
     <h2>Login page</h2>
-    <form>
+    <form onSubmit={onLogin}>
       <div className="id">
         <div>
           <label>ID</label>
         </div>
         <div>
-          <input type="text" placeholder="id를 입력해주세요요" />
+          <input type="text" placeholder="id를 입력해주세요요" ref={refId}/>
         </div>
       </div>
       <div className="password">
@@ -16,7 +41,7 @@ export default function Login(){
           <label>password</label>
         </div>
         <div>
-          <input type="text" placeholder="password를 입력해주세요" />
+          <input type="password" placeholder="password를 입력해주세요" ref={refPassword}/>
         </div>
       </div>
       <button>Login</button>
